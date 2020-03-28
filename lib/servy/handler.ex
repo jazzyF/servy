73d5c713664cf83945c6a81184967e7c6882114e
jazzyF <folabi.ayinde@gmail.com>
2020-03-28 13:01:00 -0400

    08

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,10 +2,33 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_request
+    |> rewrite_path
     |> log
     |> route
+    |> emojify
+    |> track
     |> format_response
   end
+
+  def emojify(%{status: 200, resp_body: body} = conv) do
+    %{conv | resp_body: "emoji #{body} emoji"}
+  end
+
+  def emojify(conv), do: conv
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts "Warning #{path} is on the loose!"
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  def rewrite_path(conv), do: %{ conv | path: "/wildthings" }
 
   def log(conv), do: IO.inspect conv
 
@@ -22,23 +45,29 @@ defmodule Servy.Handler do
       status: nil }
   end
 
-  def route(conv) do
-    route(conv, conv.method, conv.path)
+  # def route(conv) do
+  #   route(conv, conv.method, conv.path)
+  # end
+
+  def rewrite_request(%{method: "GET", path: "/bears?id=" <> id} = conv) do
+    %{conv | path: "/bears/" <> id }
   end
 
-  def route(conv, "GET", "/wildthings") do
+  def rewrite_request(conv), do: conv
+
+  def route(%{method: "GET", path: "/wildthings"} = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
-  def route(conv, "GET", "/bears") do
+  def route(%{method: "GET", path: "/bears"} = conv) do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
-  def route(conv, "GET", "/bears/" <> id) do
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 
-  def route(conv, _method, path) do
+  def route(%{path: path} = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
 
@@ -65,16 +94,8 @@ defmodule Servy.Handler do
 
 end
 
-# request = """
-# GET /wildthings HTTP/1.1
-# Host: example.com
-# User-Agent: ExampleBrowser/1.0
-# Accept: */*
-
-# """
-
 request = """
-GET /bears/1 HTTP/1.1
+GET /bears?id=1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
